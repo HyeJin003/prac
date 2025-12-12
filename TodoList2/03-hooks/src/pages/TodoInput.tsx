4829c10e9c6dedddf52e1407dc59a6b3e9914a46
HyeJin003 <rkddkwnl003@gmail.com>
2025-12-12 22:52:00 +0900

    프로젝트 세팅 및 학습 내용 추가
    - TodoList2 기능 업데이트
    - jhBlog 프로젝트 추가 (Supabase 연동)
    - 학원복습 todoList 프로젝트 추가
    - .gitignore 설정 추가

--- TodoList2/03-hooks/src/pages/TodoInput.tsx
+++ TodoList2/03-hooks/src/pages/TodoInput.tsx
@@ -1,33 +1,46 @@
-import { useState, type KeyboardEvent } from "react";
+import { useRef, useState, type KeyboardEvent } from "react";
 
 interface TodoInputProps {
   addItem: (title: string) => void;
 }
 
 // TODO 1. useRef를 사용해 새로운 아이템이 추가된 후 input 요소에 포커스가 갈수 있게 처리
-function TodoInput({ addItem }: TodoInputProps){
-  console.log('\t\tTodoInput 렌더링');
+function TodoInput({ addItem }: TodoInputProps) {
+  console.log("\t\tTodoInput 렌더링");
 
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
+
+  //해당 타입의 객체를 가리키는 ref를 생성하고, 이 객체는 null로 초기화 한다.
+  const inputRef = useRef<HTMLInputElement>(null);
 
   // 추가 버튼 클릭 이벤트 핸들러
   const handleAdd = () => {
-    if(title.trim() !== ''){
+    if (title.trim() !== "") {
       addItem(title);
-      setTitle('');
+      setTitle("");
+      inputRef.current?.focus();
     }
   };
 
   // 엔터 이벤트 핸들러
   const handleAddKeydown = (event: KeyboardEvent) => {
-    if(event.nativeEvent.isComposing) return; // 글자가 완성되지 않았을 경우 무시한다.(Mac 사용자)
-    if(event.key === 'Enter') handleAdd();
+    if (event.nativeEvent.isComposing) return; // 글자가 완성되지 않았을 경우 무시한다.(Mac 사용자)
+    if (event.key === "Enter") handleAdd();
   };
 
   return (
     <div className="todoinput">
-      <input type="text" autoFocus value={ title } onChange={ (e) => setTitle(e.target.value) } onKeyDown={ handleAddKeydown } />
-      <button type="button" onClick={ handleAdd }>추가</button>
+      <input
+        type="text"
+        ref={inputRef}
+        autoFocus
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        onKeyDown={handleAddKeydown}
+      />
+      <button type="button" onClick={handleAdd}>
+        추가
+      </button>
     </div>
   );
 }
